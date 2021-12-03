0efd66d33e73af402b0c7077f88d5ca98f7649f7
Gizmo-30 <anvaradizov80@gmail.com>
2021-12-03 12:49:42 +0500

    first

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-let a = prompt('Введите имя')
-let b = +prompt('Введите год рождения')
-let c = +prompt('Введите нынешниий год')
+do {
+    var a = prompt('Введите имя')
+} while (!isNaN(a));
+do {
+    var b = +prompt('Введите год рождения')
+} while (isNaN(b));
+do {
+    var c = +prompt('Введите нынешниий год')
+} while (isNaN(c));
 
 function age(a,b,c) {
     let fn = (a + ', ваш возраст ' + (c-b) )
